b441037bdde8073f6aa77b771f2c6b15c4c07b1d
Eric Geldmacher <egeldmacher@wustl.edu>
2020-10-27 16:16:46 -0500

    add license

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ericgeldmacher/hwaddress",
-    license="GPLv3",
+    license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
