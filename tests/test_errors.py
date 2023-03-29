54cf5ad24917d310cdc35e43aae58043ac562e35
Eric Geldmacher <egeldmacher@wustl.edu>
2023-03-29 14:28:20 -0500

    Format with black

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,8 +1,7 @@
 """unittest to test that expected errors get raised."""
 
 import unittest
-from hwaddress import MAC, new_hwaddress_class, \
-                      get_verifier, get_address_factory
+from hwaddress import MAC, new_hwaddress_class, get_verifier, get_address_factory
 
 
 class RaiseError(unittest.TestCase):
@@ -11,24 +10,26 @@ class RaiseError(unittest.TestCase):
     class AttrErr(MAC):
         """Class with attribute error."""
 
-        _len_ = 'Potato'
+        _len_ = "Potato"
 
     def test_attr_error(self):
         """Test that AttributeError is raised in given conditions."""
-        modellist = [('_', 47, '.', 2, False),
-                     ('_', 48, 3, 2, False),
-                     ('_', 48, '.', '2', False),
-                     ('_', 48, '.', 2, 'Potato')]
+        modellist = [
+            ("_", 47, ".", 2, False),
+            ("_", 48, 3, 2, False),
+            ("_", 48, ".", "2", False),
+            ("_", 48, ".", 2, "Potato"),
+        ]
 
         for model in modellist:
             self.assertRaises(AttributeError, new_hwaddress_class, *model)
 
-        self.assertRaises(AttributeError, self.AttrErr, '12:34:56:78:90:ab')
+        self.assertRaises(AttributeError, self.AttrErr, "12:34:56:78:90:ab")
 
     def test_type_error(self):
         """Test that TypeError is raised in given conditions."""
         strerrlist = [1, True, 4.4, {}, (), [], MAC]
-        interrlist = strerrlist[2:] + ['str']
+        interrlist = strerrlist[2:] + ["str"]
         macerrlist = strerrlist[:-1]
         strictlist = [5, list(), list, str]
 
@@ -39,7 +40,7 @@ class RaiseError(unittest.TestCase):
             self.assertRaises(TypeError, MAC.verify, ei)
 
         for ei in interrlist:
-            self.assertRaises(TypeError, new_hwaddress_class, '_', length=ei)
+            self.assertRaises(TypeError, new_hwaddress_class, "_", length=ei)
 
         for ei in macerrlist:
             self.assertRaises(TypeError, get_verifier, ei)
@@ -48,10 +49,10 @@ class RaiseError(unittest.TestCase):
             self.assertRaises(TypeError, get_address_factory, ei)
 
         for ei in strictlist:
-            self.assertRaises(TypeError, MAC.strict, '', verifier=ei)
+            self.assertRaises(TypeError, MAC.strict, "", verifier=ei)
 
     def test_value_error(self):
         """Test that ValueError is raised in given conditions."""
-        self.assertRaises(ValueError, get_address_factory(), 'abcd')
-        self.assertRaises(ValueError, MAC, '12:34:56:78:90:ag')
-        self.assertRaises(ValueError, MAC.strict, '12-34-56-78-90-ab')
+        self.assertRaises(ValueError, get_address_factory(), "abcd")
+        self.assertRaises(ValueError, MAC, "12:34:56:78:90:ag")
+        self.assertRaises(ValueError, MAC.strict, "12-34-56-78-90-ab")
